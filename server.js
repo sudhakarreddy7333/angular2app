3e94065a23305078271b69b8578f4bff3645cf45
sudhakar reddy <sudhakar.balreddy@mindtree.com>
2017-06-01 08:53:50 +0530

    port initialization

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
-app.listen('5700', function  (err,res) {
+var port = process.env.PORT || 5700;
+app.listen(port, function  (err,res) {
 	if(err){
     console.log(err);
     };
